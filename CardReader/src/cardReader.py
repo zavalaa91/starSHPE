a8bc8a3bb19a17cc359ed93d657aadfa41baa577
zavalaa91 <zavalaa91@gmail.com>
2012-10-13 14:16:35 -0500

    Fully working card swiper for GBMs

--- CardReader/src/cardReader.py
+++ CardReader/src/cardReader.py
@@ -13,16 +13,15 @@ def checkIfExists(myUIN, myCursor):
     '''
     
     myCursor.execute('SELECT UIN FROM members')
-    
     for storedUIN in myCursor.fetchall():
-        if storedUIN == myUIN:
+        if str(storedUIN)[1:-3] == str(myUIN):
             return True
     return False
 
 def connectToDB():
     '''This functions connects to the SHPE shpeuiuc_members database.
     '''
-    db=MySQLdb.connect(host='127.0.0.1',user='shpeuiuc_shpeuiuc_2012',passwd='xtTjlqno',db='shpeuiuc_shpeuiuc_members')
+    db=MySQLdb.connect(host='174.132.129.8',user='shpeuiuc_2012',passwd='xtTjlqno',db='shpeuiuc_members',port=3306)
     myCursor = db.cursor()
     return myCursor
 
@@ -31,14 +30,14 @@ def updateGBMAttendance(UIN, myCursor):
     @param UIN: a University Identification Number in integer format.
     @param myCursor: a cursor for the SHPE members database.
     '''
-    myCursor.execute('UPDATE members SET gbmsAttended=gbmsAttended+1 WHERE UIN=%d',UIN)
+    myCursor.execute('UPDATE members SET gbmsAttended=gbmsAttended+1 WHERE UIN=%d' % UIN)
     
 def firstAttendance(UIN, myCursor):
     '''This function increases the members gbm attendance by 1.
     @param UIN: a University Identification Number in integer format.
     @param myCursor: a cursor for the SHPE members database.
     '''
-    myCursor.execute('INSERT INTO members (UIN,gbmAttended) VALUES (%d,%d)',UIN,1)
+    myCursor.execute('INSERT INTO members (UIN,gbmsAttended) VALUES (%d,%d)' % (UIN,1))
 
 def main():
     while(True):
@@ -49,6 +48,7 @@ def main():
             UIN = int(cardString[5:-16])
         except ValueError:
             print 'Given UIN is not in the correct format'
+            return
         print UIN
         myCursor = connectToDB()
         if checkIfExists(UIN,myCursor):
